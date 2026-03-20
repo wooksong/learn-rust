aac6b8b0c0f0936e37652becb6996d14f0c842d1
Wook Song <wook.song@naverlabs.com>
2026-03-20 21:15:57 +0900

    Rustlings: Complete quiz exercises
    - Complete quiz1 ~ quiz3 (Cumulative Rustlings quizzes)
    
    Total: 3 exercises completed

---
+++ Rustlings/exercises/quizzes/quiz1.rs
@@ -0,0 +1,23 @@
+fn calculate_price_of_apples(n_apples: i32) -> i32 {
+    let price = if n_apples > 40 { 1 } else { 2 };
+
+    n_apples * price
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+// Don't change the tests!
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_test() {
+        assert_eq!(calculate_price_of_apples(35), 70);
+        assert_eq!(calculate_price_of_apples(40), 80);
+        assert_eq!(calculate_price_of_apples(41), 41);
+        assert_eq!(calculate_price_of_apples(65), 65);
+    }
+}
